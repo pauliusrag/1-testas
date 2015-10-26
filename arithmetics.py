21678bdde77c0332c0c71943d7b70d8d41bf97fa
Paulius Ragauskas <pauliusrag@gmail.com>
2015-10-26 14:45:42 +0200

    kazkas

--- arithmetics.py
+++ arithmetics.py
@@ -1,12 +1,12 @@
 
 def add(a, b):
-    return a + (b - 1) 
+    return a + b 
 
 def subtract(a, b):
-    return a * b + a / b
+    return a-b
 
 def multiply(a, b):
-    return a ** b
+    return a * b
 
 def devide(a, b):
-    return a / b + 2
+    return a / b
